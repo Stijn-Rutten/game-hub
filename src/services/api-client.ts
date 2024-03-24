5f91902f7f11be86abe449b5d76fd2d6ab4a6c63
Stijn Rutten <s.rutten@agrifirm.com>
2024-03-24 17:57:45 +0100

    Refactor: remove api key from source

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api/",
   params: {
-    key: "0997cb2b4173461688e47a4e75196c17",
+    key: import.meta.env.VITE_API_KEY,
   },
 });
