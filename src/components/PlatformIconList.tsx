37a46d5f27225552e7022addb41dc7fbbd18a503
Stijn Rutten <s.rutten@agrifirm.com>
2024-03-24 13:37:31 +0100

    Display platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,42 @@
+import {
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons/lib";
+
+function PlatformIconList({ platforms }: { platforms: Platform[] }) {
+  const iconMap: { [key: string]: IconType } = {
+    playstation: FaPlaystation,
+    pc: FaWindows,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    nintendo: SiNintendo,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    android: FaAndroid,
+  };
+
+  return (
+    <HStack marginY={3}>
+      {platforms.map((platform) => (
+        <Icon
+          color="gray.500"
+          key={platform.slug}
+          as={iconMap[platform.slug]}
+        />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
